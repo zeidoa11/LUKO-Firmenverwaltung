5682d78799c6e0e6515cc90e4ad7b801868f6d11
Enis Lushtaku <Enis Lushtaku@10.100.126.27>
2015-05-08 08:17:31 +0200

    Adresse klasse wurde erstellt

---
+++ src/beans/Adresse.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package beans;
+
+/**
+ *
+ * @author Enis Lushtaku
+ */
+public class Adresse
+{
+    private String strasse;
+    private int hausnummer;
+    private int stock;
+    private int tür;
+    private int plz;
+    private String stadt;
+}
