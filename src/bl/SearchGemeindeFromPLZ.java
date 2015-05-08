5ae4dbfce6c86040a58f7c73d756be589f7dbcf5
David <David@10.100.126.57>
2015-05-08 08:29:13 +0200

    file-Pajckage und SearchGemeindeFromPLZ erzeugt
    David

---
+++ src/bl/SearchGemeindeFromPLZ.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package bl;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+
+/**
+ *
+ * @author David
+ */
+public class SearchGemeindeFromPLZ
+{
+    private final String path = System.getProperty("user.dir")
+            +File.separator+"src"
+            +File.separator+"files"
+            +File.separator+"";
+    private File file;
+    private FileReader fReader;
+    private BufferedReader bReader;
+    
+}
