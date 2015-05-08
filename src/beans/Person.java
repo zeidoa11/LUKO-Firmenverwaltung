be44141fa2f992461e61bde11222629350b3cbdb
David <David@10.100.126.57>
2015-05-08 08:18:17 +0200

    Klasse Person, Mitarbeiter und Kund erstellt
    David

---
+++ src/beans/Person.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package beans;
+
+/**
+ *
+ * @author David
+ */
+public class Person
+{
+    private String vorname;
+    private String nachname;
+    private Adresse adresse;
+    
+    
+}
